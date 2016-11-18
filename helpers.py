b1345c8e83848b11d38dbf87cde731f0a11fe38d
Max Tyler <mtyler88@gmail.com>
2016-11-18 13:30:55 +0000

    Make pymodoro work with lemonbar

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+import datetime
+
 FNV_PRIME=16777619
 FNV_OFFSET_BASIS=2166136261
 
@@ -9,9 +11,24 @@ def fnv1a(bytes_to_hash):
         hashed&=0xffffffff
     return hashed.to_bytes(4, "big")
 
+def sec_to_MS(secs):
+    return datetime.datetime.fromtimestamp(secs).strftime('%M:%S')
+
+def sec_to_HMS(secs):
+    if secs>=3600:
+        return datetime.datetime.fromtimestamp(secs).strftime('%H:%M:%S')
+    else: return datetime.datetime.fromtimestamp(secs).strftime('%M:%S') 
+
 HASH_NAME=fnv1a(b"pymodoro")
 
-TEXT_CMD=["status", "next_phase"]
+TEXT_CMD=["status", "next_phase", "stop", "add_time"]
+
+MIN_UP="~/git/Pymodoro/pymodoro-cli -mu"
+MIN_DOWN="~/git/Pymodoro/pymodoro-cli -md"
+SEC_UP="~/git/Pymodoro/pymodoro-cli -su"
+SEC_DOWN="~/git/Pymodoro/pymodoro-cli -sd"
+NEXT_PHASE="~/git/Pymodoro/pymodoro-cli -n"
+E_CM="%{A}"
 
 BYTE_CMD={}
 for key in TEXT_CMD:
